2395405599a28958ccbf4270ca752f273d03052f
dyk00 <dayeongkim1@gmail.com>
2025-01-15 19:18:18 +0100

    updated for exploring and printing subgroups

--- emm/description.py
+++ emm/description.py
@@ -2,3 +2,29 @@
 # to prevent duplicates when inserting a new description
 def sort_description(description):
     return sorted(description, key=lambda x: (x[0], str(x[1])))
+
+
+# to print a list of descriptions in a readable format
+def description_to_string(description):
+
+    # initalize string
+    str = []
+
+    # for each variable and value in the description
+    for (var, value) in description:
+
+        # if the value is a boolean
+        if isinstance(value, bool):
+            str.append(f"{var} == {value}")
+
+        # if the value is a numeric range
+        elif isinstance(value, tuple):
+            l, r = value
+            str.append(f"{var} in [{l}, {r})")
+
+        # if the value is a single numeric value
+        else:
+            str.append(f"{var} == {value}")
+
+    # return the combined string
+    return " ^ ".join(str)
--- main.py
+++ main.py
@@ -4,37 +4,62 @@ from data_processing.data_io import read_parquet
 # emm
 from emm.discretization import discretize_numeric_cols
 from emm.emm_beam import emm_beam
-from emm.quality_measure import quality_measure
-from emm.refinement import refinement_operator
+from emm.description import description_to_string
 
 # survival analysis
 
 
 def main():
+    df, cols = read_parquet("./data", "example_data_with_prob.parquet")
 
-    df, cols = read_parquet("./data", "example_data.parquet")
-
+    # just in case columns are still in boolean type,
+    # but make sure there are no missing values
     bool_cols = df.select_dtypes(include=["bool", "boolean"]).columns
-
     df[bool_cols] = df[bool_cols].astype(bool)
 
-    target_columns = ["time_to_first_event", "avg_survival_probability"]
+    # target variables are same as survival analysis ones
+    target_columns = ["time_to_event", "is_first"]
 
-    feature_columns = [c for c in cols if c not in target_columns]
+    # to exclude from features
+    quality_columns = ["avg_survival_probability"]
 
-    col_bins = discretize_numeric_cols(df, feature_columns, bins_count=2)
+    # include all except for target and quality
+    feature_columns = [
+        c for c in cols if c not in target_columns and c not in quality_columns
+    ]
 
-    constraints = 3
+    # discretize numerical columns
+    col_bins = discretize_numeric_cols(df, feature_columns, bins_count=5)
 
+    # define constraints (e.g. min_size, max_size)
+    constraints = {"min_size": 3}
+
+    # run emm_beam to get the result set
     result_set = emm_beam(
         dataset=df,
-        quality_measure=quality_measure,
-        refinement_operator=refinement_operator,
-        w=2,
-        d=2,
-        q=2,
+        columns=feature_columns,
+        col_bins=col_bins,
+        # beam width
+        w=8,
+        # maximum depth to explore
+        d=3,
+        # how many subgroups in result set
+        q=5,
         constraints=constraints,
     )
+
+    # if no subgroups are found
+    if not result_set.heap:
+        print("No subgroups that satisfy the constraints.")
+        return
+
+    # print the q numbers of subgroups that have the lowest survival probability on average
+    print("\n===== Q-Subgroups with the Lowest Survival Probability =====")
+    for quality, description in sorted(result_set.heap, reverse=True):
+        desc = description_to_string(description)
+        print(
+            f"Description: {desc}, Quality (Average Survival Probability): {-quality:.4f}"
+        )
 
 
 if __name__ == "__main__":
