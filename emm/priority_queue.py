c60ae98af6c4622a636829fccdae9797aa67db64
dyk00 <dayeongkim1@gmail.com>
2025-01-15 14:14:10 +0100

    modified insertion, updated duplicates prevention

--- emm/priority_queue.py
+++ emm/priority_queue.py
@@ -1,4 +1,5 @@
 import heapq
+from .description import sort_description
 
 
 class PriorityQueue:
@@ -6,36 +7,40 @@ class PriorityQueue:
         self.max_size = max_size
         self.heap = []
 
-    # insert a tuple (priority, description) into min-heap
-    def insert_with_priority(self, description, priority):
-        tuple = (priority, description)
-        heapq.heappush(self.heap, tuple)
+        # maintain a set for removing duplicate
+        self.seen = set()
 
-        # if exceeds the max size, remove description with largest priority
-        # (i.e., quality measure)
-        if self.max_size is not None and len(self.heap) > self.max_size:
+    # insert a tuple (-priority, description) into min-heap
+    # negative priority is easy to handle
+    # when removing description with actual highest probability
+    def insert_with_priority(self, description, quality):
+        priority = -quality
+        sorted_desc = tuple(sort_description(description))
 
-            # single description
-            largest = heapq.nlargest(1, self.heap)[0]
-            self.heap.remove(largest)
+        # if description is not in seen, push to heap and seen
+        if sorted_desc not in self.seen:
+            heapq.heappush(self.heap, (priority, sorted_desc))
+            self.seen.add(sorted_desc)
 
-            # reheapify
-            heapq.heapify(self.heap)
+            # if exceeds the max size, remove description with acutal highest probability
+            # (i.e., quality measure)
+            # as we're focusing on finding descriptions with lowest probability
+            if self.max_size is not None and len(self.heap) > self.max_size:
+                removed = heapq.heappop(self.heap)
+                self.seen.remove(removed[1])
 
     # return True if the heap is empty
     def empty(self):
         return len(self.heap) == 0
-
-    # # return the description with smallest priority
-    # def get_front_element(self):
-    #     if self.empty():
-    #         return None
-    #     # [][]: [order of heap], [priority/description]
-    #     return self.heap[0][1]
 
     # return and remove the description with smallest priority
     def get_pop_front_element(self):
         if self.empty():
             return None
         priority, description = heapq.heappop(self.heap)
+        self.seen.remove(description)
         return description
+
+    # making heap iterable for printing in main
+    def __iter__(self):
+        return iter(self.heap)
