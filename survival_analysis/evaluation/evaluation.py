7830975fab8980ac8243307872c59fc236053eb1
dyk00 <dayeongkim1@gmail.com>
2025-01-28 12:44:54 +0100

    added c index for sksurv

--- survival_analysis/evaluation/evaluation.py
+++ survival_analysis/evaluation/evaluation.py
@@ -7,12 +7,17 @@ from sksurv.metrics import concordance_index_ipcw
 # get concordance index on test set
 # if the predicted scores are risks/hazards, multiply by -1
 # https://lifelines.readthedocs.io/en/latest/lifelines.utils.html
-def get_c_index(test_df, duration_col, event_col, hazard_scores):
+def get_c_index_lifelines(test_df, duration_col, event_col, hazard_scores):
     return concordance_index(
         test_df[duration_col],
         -hazard_scores,
         test_df[event_col],
     )
+
+
+# get the c index using sksurv package
+def get_c_index_sksurv(estimator, X_test, y_test):
+    return estimator.score(X_test, y_test)
 
 
 # get concordance index based on inverse probability of censoring weights
