c88f219ec56127d7f57139170f4f042a8bb052be
dyk00 <dayeongkim1@gmail.com>
2025-01-28 15:13:20 +0100

    added rsf and ersf. gotta combine with gb

---
+++ survival_analysis/models/rsf.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sksurv.ensemble import RandomSurvivalForest, ExtraSurvivalTrees
+
+# fit random survival forest
+def fit_rsf(
+    X_train,
+    y_train,
+    n_estimators=100,
+    random_state=42,
+):
+    rsf = RandomSurvivalForest(
+        n_estimators=n_estimators,
+        random_state=random_state,
+        n_jobs=-1,
+    )
+    rsf.fit(X_train, y_train)
+    return rsf
+
+
+# fit extremely random survival forest
+def fit_ersf(
+    X_train,
+    y_train,
+    n_estimators=100,
+    random_state=42,
+):
+    ersf = RandomSurvivalForest(
+        n_estimators=n_estimators,
+        random_state=random_state,
+        n_jobs=-1,
+    )
+    ersf.fit(X_train, y_train)
+    return ersf
+
+
+# predict hazard (risk) scores
+def predict_hazard_sksurv(estimator, X_test):
+    prediction = estimator.predict(X_test)
+    return prediction
